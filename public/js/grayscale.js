e83a73a2388fa87381acde8fe9b81c6addea3a56
Roger Zurawicki <rzurawicki@gmail.com>
2014-11-07 20:18:35 -0500

    Website gets layout

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -18,7 +18,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - $('nav').height()
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -50,114 +50,7 @@ function init() {
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
-    //    styles: [{
-    //        "featureType": "water",
-    //        "elementType": "geometry",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 17
-    //        }]
-    //    }, {
-    //        "featureType": "landscape",
-    //        "elementType": "geometry",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 20
-    //        }]
-    //    }, {
-    //        "featureType": "road.highway",
-    //        "elementType": "geometry.fill",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 17
-    //        }]
-    //    }, {
-    //        "featureType": "road.highway",
-    //        "elementType": "geometry.stroke",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 29
-    //        }, {
-    //            "weight": 0.2
-    //        }]
-    //    }, {
-    //        "featureType": "road.arterial",
-    //        "elementType": "geometry",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 18
-    //        }]
-    //    }, {
-    //        "featureType": "road.local",
-    //        "elementType": "geometry",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 16
-    //        }]
-    //    }, {
-    //        "featureType": "poi",
-    //        "elementType": "geometry",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 21
-    //        }]
-    //    }, {
-    //        "elementType": "labels.text.stroke",
-    //        "stylers": [{
-    //            "visibility": "on"
-    //        }, {
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 16
-    //        }]
-    //    }, {
-    //        "elementType": "labels.text.fill",
-    //        "stylers": [{
-    //            "saturation": 36
-    //        }, {
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 40
-    //        }]
-    //    }, {
-    //        "elementType": "labels.icon",
-    //        "stylers": [{
-    //            "visibility": "off"
-    //        }]
-    //    }, {
-    //        "featureType": "transit",
-    //        "elementType": "geometry",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 19
-    //        }]
-    //    }, {
-    //        "featureType": "administrative",
-    //        "elementType": "geometry.fill",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 20
-    //        }]
-    //    }, {
-    //        "featureType": "administrative",
-    //        "elementType": "geometry.stroke",
-    //        "stylers": [{
-    //            "color": "#000000"
-    //        }, {
-    //            "lightness": 17
-    //        }, {
-    //            "weight": 1.2
-    //        }]
-    //    }]
+
     };
 
     // Get the HTML DOM element that will contain your map 
