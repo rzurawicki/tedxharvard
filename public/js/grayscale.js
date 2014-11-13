53289588aef320ea2d61d71311141d0b974c72f8
Roger Zurawicki <rzurawicki@gmail.com>
2014-11-13 04:16:12 -0500

    Added team content
    moved some javascript around

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -5,7 +5,7 @@
  */
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
@@ -14,8 +14,8 @@ $(window).scroll(function() {
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top - $('nav').height()
@@ -25,45 +25,17 @@ $(function() {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
 
-// Google Maps Scripts
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
+$('.thumbnail').hover(
+    function () {
+        $(this).find('.caption').slideDown(250);
+        $('.caption-btm').hide();
+    },
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 15,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(42.375833, -71.1147449), // Sanders Theatre
-
-        // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var myLatLng = new google.maps.LatLng(42.3758795, -71.1145451);
-    new google.maps.Marker({
-        position: myLatLng,
-        map: map
+    function () {
+        $(this).find('.caption').slideUp(250);
+        $('.caption-btm').show();
     });
-}
