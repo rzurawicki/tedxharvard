9872310f00dc3a5bdb1063cab7d3e557b8a71dff
Roger Zurawicki <rzurawicki@gmail.com>
2014-11-24 23:14:48 -0500

    homepage with parallax

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -5,8 +5,8 @@
  */
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function () {
-    if ($(".navbar").offset().top > 50) {
+$('.parallax').scroll(function () {
+    if ($("#intro").offset().top < -100) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
